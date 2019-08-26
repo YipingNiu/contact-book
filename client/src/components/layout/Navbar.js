99b9a06e3d17cc9116dd7d59d7d97f8e2edb5637
Nndou <yp.niu@foxmail.com>
2019-08-26 11:05:52 +1000

    Feat:bulid navbar and router setup

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className='navbar bg-info text-light navbar-expand'>
+      <div className='container-fluid'>
+        <h1 className='mb-0'>
+          <i className={icon} />
+          {title}
+        </h1>
+        <ul className='navbar-nav'>
+          <li className='nav-item'>
+            <Link className='nav-link text-light' to=''>
+              Home
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link className='nav-link text-light' to='/about'>
+              About
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string
+};
+
+Navbar.defaultProps = {
+  title: "Contact Book",
+  icon: "fas fa-id-card-alt"
+};
+
+export default Navbar;
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className='my-1'>
+        This is a full stack React app for keeping contacts
+      </p>
+      <p className='bg-dark text-light py-1 my-3'>
+        <strong className='ml-2'>Version:</strong>1.0.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
