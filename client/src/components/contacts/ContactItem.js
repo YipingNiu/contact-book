23719a40d10e433c541c6fbec9e570db59e790d2
Nndou <yp.niu@foxmail.com>
2019-08-29 23:50:23 +1000

    add ContactItem component

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 const ContactItem = ({ contact }) => {
+  const { id, name, email, phone, type } = contact;
   return (
     <div className='card bg-light'>
-      <h3></h3>
+      <h3 className='text-primary text-left'>
+        {name}{" "}
+        <span
+          className={
+            "badge" +
+            (type === "professional" ? "badge-success" : "badge-primary")
+          }
+        >
+          {type}
+        </span>
+      </h3>
     </div>
   );
 };
