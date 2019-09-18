bc524bce373a361a9fe91414c76d5e3393f6a6f1
Nndou <yp.niu@foxmail.com>
2019-09-19 02:19:53 +1000

    Feat:Change background to particles

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,8 @@ import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
 import AlertState from "./context/alert/AlertState";
 import setAuthToken from "./utils/setAuthToken";
+import Particles from "react-particles-js";
+import particlesOptions from "./utils/particlesOptions";
 
 //If have token,login the user
 if (localStorage.token) {
@@ -28,25 +30,24 @@ const App = () => {
         <ContactState>
           <Router>
             <Fragment>
-              <div className='main-overlay'>
-                <Navbar />
-                <div className='container'>
-                  <div className='row' style={{ height: "100vh" }}>
-                    <div
-                      className='col-12 my-auto p-0'
-                      style={{
-                        height: "750px",
-                        boxShadow: "10px 10px 30px #888888"
-                      }}
-                    >
-                      <Alerts />
-                      <Switch>
-                        <PrivateRoute exact path='/' component={Home} />
-                        <Route exact path='/about' component={About} />
-                        <Route exact path='/register' component={Register} />
-                        <Route exact path='/login' component={Login} />
-                      </Switch>
-                    </div>
+              <Particles className='particles' params={particlesOptions} />
+              <Navbar />
+              <div className='container'>
+                <div className='row' style={{ height: "100vh" }}>
+                  <div
+                    className='col-12 my-auto p-0'
+                    style={{
+                      height: "750px",
+                      boxShadow: "10px 10px 30px #888888"
+                    }}
+                  >
+                    <Alerts />
+                    <Switch>
+                      <PrivateRoute exact path='/' component={Home} />
+                      <Route exact path='/about' component={About} />
+                      <Route exact path='/register' component={Register} />
+                      <Route exact path='/login' component={Login} />
+                    </Switch>
                   </div>
                 </div>
               </div>
